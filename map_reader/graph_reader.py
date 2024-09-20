eba136ec988c3ce8ae0ef84aadd1e058270a8ed5
M.T. Wilk <m.t.wilk@umail.leidenuniv.nl>
2024-09-20 20:21:20 +0200

    fixed a minor mistake in speed_limit

--- map_reader/graph_reader.py
+++ map_reader/graph_reader.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 class GeoGraph:
     def __init__(self, geojson_file):
         """
-        Initializes the GeoGraph object by reading the GeoJSON file and creating the graph.
+        Initializes the GeoGraph object by reading the GeoJSON file and creating the graph
         
         Parameters:
-        geojson_file (str): Path to the GeoJSON file.
+        geojson_file (str): Path to the GeoJSON file
         """
         self.geojson_file = geojson_file
         self.gdf = gpd.read_file(geojson_file)
@@ -17,25 +17,25 @@ class GeoGraph:
 
     def _create_graph(self):
         """
-        Creates a graph from the GeoDataFrame by adding edges based on the geometry.
+        Creates a graph from the GeoDataFrame by adding edges based on the geometry
         """
         for _, row in self.gdf.iterrows():
             geometry = row.geometry
-            speed_limit = row.get('maxspeed', None)
+            speed_limit = row.get('maxspeed', 30)
             
             if geometry.geom_type == 'LineString':
                 coords = list(geometry.coords)
                 for i in range(len(coords) - 1):
                     start = coords[i]
                     end = coords[i + 1]
-                    self.G.add_edge(start, end, speed_limit=30)
+                    self.G.add_edge(start, end, speed_limit=speed_limit)
 
     def __repr__(self):
         """
-        Represents the graph as a string showing nodes and edges.
+        Represents the graph as a string showing nodes and edges
         
         Return types:
-        str: A string representation of the graph.
+        str: A string representation of the graph
         """
         nodes = list(self.G.nodes)
         edges = list(self.G.edges(data=True))
@@ -43,7 +43,7 @@ class GeoGraph:
 
     def visualize(self):
         """
-        Visualizes the graph using matplotlib with nodes in green and edges in red.
+        Visualizes the graph using matplotlib with nodes in green and edges in red
         """
         pos = {node: node for node in self.G.nodes}
         nx.draw(self.G, pos, with_labels=False, node_size=1, font_size=1, node_color='g', edge_color='r')
