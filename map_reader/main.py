3a5f016b961ea0030f1542f913266824ecc98afe
M.T. Wilk <m.t.wilk@umail.leidenuniv.nl>
2024-11-10 16:15:53 +0100

    Upload New File

---
+++ map_reader/main.py
@@ -0,0 +1,23 @@
+"""
+rough framework for the main file of the project
+
+"""
+
+def send_neighbours():
+    """
+    calls generate inputs and sends to the UI a geojson file with them
+    """
+    pass
+    
+
+def send_score():
+    """
+    sends the score to the UI in a geojson file
+    """
+    pass
+
+def send_start():
+    """
+    sends the start, end and blocked nodes to the UI in a geojson file
+    """
+    pass
