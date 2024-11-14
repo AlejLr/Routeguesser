6073fdc499f958f77492b945b4595fe8d46b5cd6
M.T. Wilk <m.t.wilk@umail.leidenuniv.nl>
2024-11-14 19:45:05 +0100

    updated Map with correct return types for all of the functions

--- map_reader/Map.py
+++ map_reader/Map.py
@@ -1,5 +1,5 @@
-import graph_reader as gr
-
+import numpy as np
+import networkx as nx
 class Map:
     """
     A map is a dynamic data structure.
@@ -8,53 +8,85 @@ class Map:
     The Map will be able to export data in a presentable manner for the UI.
     (CONSIDER) The greater Map will be composed by multiple smaller Maps so that we can work in chunks based on zoom in.
     """
-    def __init__(self):
+    def __init__(self, Graph):
         """
         also generates the blocked nodes
         """
-        self.G = gr.GeoGraph('map_complex.geojson')
-        self.blocked_nodes = self.generate_blocked_nodes()
+        self.Graph = Graph
+        self.difficulty = 10
+        self.number_of_blocked_nodes = 0
+        self.blocked_roads = self.generate_blocked_roads(self.difficulty)
         self.score = 0
-        self.start, self.end = self.generate_start_end(1000)
+        self.startend = self.generate_start_end(1000)
         self.current_pos = self.start
         self.chosen_path = [self.start]
-        self.optimal_path = self.astar(10)
+        self.optimal_path = self.astar(self.difficulty)
 
 
     def generate_blocked_roads(self, number_of_blocked_nodes):
-        """generates blocked nodes that are roughly in the direction of the end from the start, based on the difficulty (number of blocked nodes)"""
-        pass
+        """generates blocked nodes that are roughly in the direction of the end from the start, based on the difficulty (number of blocked nodes)
+        returns a dictionary of blocked nodes, with the starting nodes for a road as a key, and intermediate nodes as values
+        Alternatively, the values can be empty and we can just use the blocked nodes
+        rtype: dict(G.node, set(G.node))
+        """
+        #TODO
+        # you can access graph nodes with G.nodes
+        # for node in np.random.choice(self.Graph.G.nodes, number_of_blocked_nodes):
+        #     pass 
+        # pass
+        # result = {}
+        # # return result
 
     def generate_start_end(self, min_distance):
-        """generate a start and end node randomly from the graph, which must have a minimum distance between them"""
-        pass
-
+        """generate a start and end node randomly from the graph, which must have a minimum distance between them
+        rtype:  dict(string, G.node)
+        like {"start": , "end": G.node}
+        """
+        #TODO
+        # also here, you can access nodes like this
+        # this is an example and does not work for now
+        nodes = list(self.Graph.G.nodes)
+    
+        print(nodes)
+        while True:
+            start, end = np.random.choice(nodes, 2, replace=False)
+            if self.calculate_cartesian_distance(start, end) >= min_distance:
+                break
+        
+        return {"start": start, "end": end}
+            
     def __repr__(self):
+        #TODO
         pass
     
     def astar(self, numberofpaths):
         """
         generate an optimal path(s) between start and end
-        rtype: dict of nodes (which contain coordinates)
+        returns: dict of nodes (which contain coordinates) and their scores, the score being the value (length of the path for now)
+        rtype: dict(set(G.node), int)
         """
+        #TODO
         pass
 
     def generate_neighbours(self, min_distance):
         """
         return graph edges based on current
-        rtype: dictionary of next nodes
+        for a curve, we want send all of the intermediate nodes
+        The key is the node we want the user to go to, the value (optional) for a curved road is the intermediate nodes
+        rtype: dict(G.node, set(G.node))
         """
+        #TODO
         pass
 
     def process_inputs(self, next_node):
         """
-        increment the score, changes the current_post
+        increment the score, changes the current_pos
         """
         self.current_pos = next_node
         if next_node not in self.path:
-            self.score += calculate_cartesian_distance(self.current_pos, self.next_node)
+            self.score += self.calculate_cartesian_distance(self.current_pos, self.next_node)
 
-
+    
     def calculate_cartesian_distance(self, node1, node2):
         """
         calculate the distance between two nodes
