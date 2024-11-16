6a132a27488082f3d3dd69fddd98d883af87e291
A. Lopez Ruiz <a.lopez.ruiz@umail.leidenuniv.nl>
2024-11-16 16:50:09 +0100

    Work in progress - initial animation and rules

--- script.js
+++ script.js
@@ -1,3 +1,66 @@
+// Animations
+/*
+const g9 = document.getElementById('g9');
+const logo = document.getElementById('logo');
+const presents = document.getElementById('presents');
+const title = document.getElementById('title');
+const subtitle = document.getElementById('subtitle');
+const startGame = document.getElementById('startGame');
+
+
+function showElement(element) {
+    element.classList.remove('hidden');
+    element.classList.add('fade-in');
+}
+
+function hideElement(element) {
+    element.classList.remove('fade-in');
+    element.classList.add('fade-out');
+    setTimeout(() => {
+        element.classList.add('hidden');
+        element.classList.remove('fade-out');
+    }, 1000);
+}
+
+
+function runAnimations() {
+
+    showElement(g9);
+
+    setTimeout(() => {
+        hideElement(g9);
+        setTimeout(() => {
+            showElement(logo);
+        }, 1000);
+    }, 2000);
+
+    setTimeout(() => {
+        hideElement(logo);
+        setTimeout(() => {
+            showElement(presents);
+        }, 1000);
+    }, 4000);
+
+    
+    setTimeout(() => {
+        hideElement(presents);
+        setTimeout(() => {
+            showElement(title);
+        }, 1000);
+    }, 6000);
+
+    setTimeout(() => {
+        hideElement(presents);
+        setTimeout(() => {
+            title.classList.add('change-Position');
+        }, 1000);
+    }, 8000);
+}
+
+document.addEventListener('DOMContentLoaded', runAnimations);
+*/
+// Rest of the functionality
+
 let distance = 0;
 let difficulty = "medium";
 let routesNum = 3;
