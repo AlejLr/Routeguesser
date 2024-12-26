34a905774585899b249ca2e1973888ceeef50b5a
L. Agbaria <l.agbaria@umail.leidenuniv.nl>
2024-12-26 22:44:17 +0100

    A file to run the multiple unit test files at the same time.

---
+++ map_reader/unit_testing/run_unittests.py
@@ -0,0 +1,9 @@
+import unittest
+
+from unittest_file_cleaner import TestFileCleaner
+from unittest_map import TestMap
+# from unittest_main import TestMain
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
