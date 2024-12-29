826d0d459654c07fcfb86a51e3de90710906778a
L. Agbaria <l.agbaria@umail.leidenuniv.nl>
2024-12-29 12:55:49 +0100

    Readjusted the unit tests for the Map class. Should be complete.

--- map_reader/unit_testing/unittest_map.py
+++ map_reader/unit_testing/unittest_map.py
@@ -2,7 +2,6 @@ import unittest
 from Map import Map
 
 import networkx as nx
-from decimal import Decimal
 
 
 class TestMap(unittest.TestCase):
@@ -11,20 +10,15 @@ class TestMap(unittest.TestCase):
     Each unit test should contain tests for functionality and type.
     """
 
-    # def setUpClass(cls):
-    #     cls.map = Map()
-
     '''
     Tests the create function, uses a collection of self-designed graph json that simulates
     the actual used json files.
     Should return a single networkX graph object.
     '''
-
     def test_create_graph(self):
         # Define results
         result_1_1 = Map._create_graph("map_test_1-1.json")
         result_1_2 = Map._create_graph("map_test_1-2.json")
-        # result_1_3 = Map._create_graph("map_test_1-3.json")  # TO-DO
 
         # Test case 1: Basic usability
         with self.subTest(msg="1.1.1) Should return a networkx graph object."):
@@ -46,19 +40,8 @@ class TestMap(unittest.TestCase):
         with self.subTest(msg="1.2.4) Should return correct number of edges."):
             self.assertEqual(3, result_1_2.number_of_edges())
 
-        # # Test case 3: Unconnected/error case  -- INCOMPLETE --
-        # with self.subTest(msg="1.3.1) Should return a networkx graph object."):
-        #     self.assertIsInstance(result_1_3, nx.Graph)
-        # with self.subTest(msg="1.3.2) Faulty graph treated as correct."):
-        #     self.assertNotEqual(nx.number_connected_components(result_1_3), 1)
-        # with self.subTest(msg="1.3.3) Should return correct number of nodes."):
-        #     self.assertEqual(result_1_3.number_of_nodes(), 2)
-        # with self.subTest(msg="1.3.4) Should return correct number of edges."):
-        #     self.assertEqual(1, result_1_3.number_of_edges())
-
         del result_1_1
         del result_1_2
-        # del result_1_3
 
     '''
     Tests the function to find a list of nodes that can be removed together while maintaining
@@ -68,20 +51,18 @@ class TestMap(unittest.TestCase):
     '''
     def test_generate_blocked_roads(self):
         # Define results
-        test_map = Map.__new__(Map)
-        test_map.Graph = nx.complete_graph(3)
-        result_2_1 = test_map.generate_blocked_roads(1)
-        test_map.Graph = nx.complete_graph(3)
+        test_map = Map('map_test_5-1.json')
+        result_2_1 = test_map.generate_blocked_roads(3)
+        test_map = Map('map_test_1-1.json')
         result_2_2 = test_map.generate_blocked_roads(2)
-        # result_2_3 = Map.generate_blocked_roads([],2)  # TO-DO?
 
         # Test case 1: Fully connected graph
         with self.subTest(msg="2.1.1) Should return a list."):
             self.assertIsInstance(result_2_1, list)
-        with self.subTest(msg="2.1.2) Should be a list of tuples."):
-            self.assertTrue(all(isinstance(x, tuple) for x in result_2_1))
+        with self.subTest(msg="2.1.2) Should be a list of roads (list[tuple])."):
+            self.assertTrue(all(isinstance(x, list) for x in result_2_1))
         with self.subTest(msg="2.1.3) Should be tuples of coordinates."):
-            self.assertTrue(all(len(x) == 2 for x in result_2_1))
+            self.assertTrue(all((len(x) == 2 and (x[0] is float and x[1] is float) for x in y) for y in result_2_1))
 
         # Test case 2: No possibilities
         with self.subTest(msg="2.2.1) Should return a list."):
@@ -89,13 +70,8 @@ class TestMap(unittest.TestCase):
         with self.subTest(msg="2.2.2) Should be an empty."):
             self.assertEqual(0, len(result_2_2))
 
-        # # Test case 3: Error case  -- INCOMPLETE --
-        # with self.subTest(msg="2.3.1"):
-        #     self.assertRaises(Exception, Map.generate_blocked_roads([],2))
-
         del result_2_1
         del result_2_2
-        # del result_2_3
         del test_map
 
     '''
@@ -106,16 +82,10 @@ class TestMap(unittest.TestCase):
         result_3_1 = Map.calculate_cartesian_distance((0, 0), (3, 4))  # MODIFY TO MAKE MORE DIFFICULT
 
         # Test case 1: Normal use
-        with self.subTest(msg="3.1.1) Should return a Decimal."):
-            self.assertIsInstance(result_3_1, Decimal)
+        with self.subTest(msg="3.1.1) Should return a float."):
+            self.assertIsInstance(result_3_1, float)
         with self.subTest(msg="3.1.2) Should return accurate result."):
             self.assertEqual(5, result_3_1)
-
-        # # Test case 2: Error handling  -- IGNORE FOR NOW --
-        # with self.subTest(msg="3.2.1) Should not accept anything other than two tuples."):
-        #     pass
-        # with self.subTest(msg="3.2.2) Should not accept any tuples of length other than 2."):
-        #     pass
 
         del result_3_1
 
@@ -138,14 +108,6 @@ class TestMap(unittest.TestCase):
         with self.subTest(msg="4.1.4) Each of the results should be a coordinate."):
             self.assertTrue(all(len(x) == 2 for x in result_4_1))
 
-        # # Test case 2: Check for exceptions and data validity
-        # with self.subTest(msg="4.2.1) Can handle too large minimum distance."):
-        #     pass
-        # with self.subTest(msg="4.2.2) Can handle all graphs."):
-        #     pass
-        # with self.subTest(msg="4.2.3) Results are unique and actual nodes."):
-        #     pass
-
         del result_4_1
         del test_map
 
@@ -163,7 +125,7 @@ class TestMap(unittest.TestCase):
 
             return test_map_.astar()
 
-        test_map = Map("map_test_5-1.json", 0)
+        test_map = Map("map_test_5-1.json")
         result_5_1 = reset(test_map)
 
         # Test case 1: Return types
@@ -173,16 +135,16 @@ class TestMap(unittest.TestCase):
             self.assertIsInstance(result_5_1[0], list)
         with self.subTest(msg="5.1.3) The list should be filled with nodes."):
             self.assertTrue(all(x in test_map.Graph for x in result_5_1[0]))
-        with self.subTest(msg="5.1.4) Second result should be a Decimal."):
-            self.assertIsInstance(result_5_1[1], Decimal)
+        with self.subTest(msg="5.1.4) Second result should be a float."):
+            self.assertIsInstance(result_5_1[1], float)
         with self.subTest(msg="5.1.5) Path should be most optimal."):
             path = [(0, 0), (7, 0), (14, 0), (21, 0)]
             self.assertEqual(4, len(result_5_1[0]))
             for i in range(4):
                 self.assertEqual(path[i], result_5_1[0][i])
             del path
-        with self.subTest(msg="5.1.6) Path length should be correct."):
-            self.assertIsInstance(result_5_1[1], Decimal)
+        with self.subTest(msg="5.1.6) Path distance should be correct."):
+            self.assertIsInstance(result_5_1[1], float)
             self.assertEqual(21, result_5_1[1])
 
         # Test case 2: Blocked roads
@@ -194,10 +156,8 @@ class TestMap(unittest.TestCase):
             for i in range(5):
                 self.assertEqual(path[i], result_5_2[0][i])
             del path
-        with self.subTest(msg="5.1.6) Path length should be correct."):
-            self.assertAlmostEqual(Decimal(22.6157731), result_5_2[1])
-
-        # Test case 3: Errors and edge cases  -- IGNORE FOR NOW --
+        with self.subTest(msg="5.2.2) Path length should be correct."):
+            self.assertAlmostEqual(22.6157731, result_5_2[1])
 
         del result_5_1
         del result_5_2
@@ -209,19 +169,25 @@ class TestMap(unittest.TestCase):
     def test_get_neighbours_and_roads(self):
         # Define results
         test_map = Map("map_test_5-1.json")
+        test_map.game_init()
 
-        result_6 = test_map.get_neighbours_and_roads()
+        result_6 = test_map.get_neighbours_and_roads(test_map.start)
 
         # Test case 1: Check return types
-        with self.subTest(msg="6.1.1) Should return a dictionary."):
-            self.assertIsInstance(result_6, dict)
-        with self.subTest(msg="6.1.2) All keys should be coordinate tuples."):
-            for x in result_6.keys():
-                self.assertIsInstance(x, tuple)
-                self.assertEqual(2, len(x))
-                self.assertTrue(all(isinstance(y, int | float | Decimal) for y in x))
-        with self.subTest(msg="6.1.3) All values should be tuples."):  # Should normalize accuracy level for unittests
-            self.assertTrue(all(isinstance(x, tuple) for x in result_6.values()))
+        with self.subTest(msg="6.1.1) Should return a list."):
+            self.assertIsInstance(result_6, list)
+        with self.subTest(msg="6.1.2) The list should contain tuples of 3 elements."):
+            self.assertTrue(all(isinstance(x, tuple) and len(x) == 3 for x in result_6))
+        with self.subTest(msg="6.1.3) First element should be a Node."):
+            self.assertIsInstance(result_6[0][0], tuple)
+            self.assertEqual(2, len(result_6[0][0]))
+            self.assertTrue(all(isinstance(x, float | int) for x in result_6[0][0]))
+        with self.subTest(msg="6.1.4) Second element should be a Road."):
+            self.assertIsInstance(result_6[0][1], list)
+            self.assertTrue(all(isinstance(x, tuple) for x in result_6[0][1]))
+            self.assertTrue(all(isinstance(x, float | int) for x in result_6[0][1][0]))
+        with self.subTest(msg="6.1.5) Third element should be a float."):
+            self.assertIsInstance(result_6[0][2], float)
 
         del result_6
         del test_map
