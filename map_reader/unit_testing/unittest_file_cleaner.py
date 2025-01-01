c5f2807702676681fb144a317f073305d66ecc3c
L. Agbaria <l.agbaria@umail.leidenuniv.nl>
2025-01-01 19:01:51 +0100

    Added the tests for the file cleaner function and extract main component

--- map_reader/unit_testing/unittest_file_cleaner.py
+++ map_reader/unit_testing/unittest_file_cleaner.py
@@ -8,9 +8,21 @@ class TestFileCleaner(unittest.TestCase):
     def test_file_cleaner(self):
         input_file = 'file_cleaner_test_1.geojson'
         output_file = 'file_cleaner_test_1.json'
+        test_file = 'file_cleaner_test_1_expected.json'
         file_cleaner(input_file, output_file)
-        # A BIT MORE COMPLEX
-        pass
+
+        with open(output_file, 'r') as a_file:
+            actual_data = json.load(a_file)
+        with open(test_file, 'r') as e_file:
+            expect_data = json.load(e_file)
+
+        self.assertEqual(expect_data, actual_data)
+
+        del actual_data
+        del expect_data
+        del input_file
+        del output_file
+        del test_file
 
     def test_euclidean_dist(self):
         # Set values.
@@ -31,7 +43,7 @@ class TestFileCleaner(unittest.TestCase):
 
     def test_dist(self):
         # Set values.
-        points = ((0, 0), (1, 1), (2, 2), (2, 4))
+        points = [(0, 0), (1, 1), (2, 2), (2, 4)]
         test_dist = dist(points)
         actual_dist = 2**1.5+2
 
@@ -45,6 +57,25 @@ class TestFileCleaner(unittest.TestCase):
 
         # Clean up.
         del points
+
+    def test_extract_main_component(self):
+        test_graph = nx.Graph()
+        test_graph.add_nodes_from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+        test_graph.add_edges_from([(0, 1), (1, 2), (2, 3), (3, 4), (4, 5)])
+        test_graph.add_edges_from([(6, 7), (7, 8), (8, 9)])
+        test_graph = extract_main_component(test_graph)
+
+        with self.subTest(msg="4.1) Should return a graph."):
+            self.assertIsInstance(test_graph, nx.Graph)
+        with self.subTest(msg="4.2) Should contain 6 nodes and 5 edges."):
+            self.assertEqual(6, test_graph.number_of_nodes())
+            self.assertEqual(5, test_graph.number_of_edges())
+        with self.subTest(msg="4.3) Should not contain the nodes or edges from the removed part."):
+            for i in range(9, 5, -1):
+                self.assertFalse(test_graph.has_node(i))
+                self.assertFalse(test_graph.has_edge(i-1, i))
+
+        del test_graph
 
 
 if __name__ == "__main__":
