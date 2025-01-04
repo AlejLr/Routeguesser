f5ca5aaf5f3b865437e7607c982c5cdcf390040d
A. Lopez Ruiz <a.lopez.ruiz@umail.leidenuniv.nl>
2025-01-04 20:11:42 +0100

    comments for script

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,5 +1,7 @@
 // Animations
-/*
+
+
+// All the constants for the anumation elements
 const g9 = document.getElementById('g9');
 const logo = document.getElementById('logo');
 const presents = document.getElementById('presents');
@@ -10,7 +12,10 @@ const content = document.querySelector('.content');
 const gameExplanation = document.querySelector('#gameExplanation');
 
 
+// Functions for the animations
 
+// When the page is loaded, an event listener is triggered
+// The elements get hidden and the start animation begins
 document.addEventListener('DOMContentLoaded', () => {
     const elements = [g9, logo, presents, title, subtitle, startGame];
     elements.forEach(element => {
@@ -21,12 +26,17 @@ document.addEventListener('DOMContentLoaded', () => {
     runAnimations();
 });
 function addStartGameEventListener() {
+    // This function adds an event listener to the start game button when the button
+    // is clicked the game is started and the function hideStartScreen is called
+
     if (startGame) {
         startGame.addEventListener('click', hideStartScreen);
     }
 }
 
 function showElement(element) {
+    // This function shows the element by removing the hidden class and adding the fade-in class
+
     if (element) {
         element.classList.remove('hidden');
         element.classList.add('fade-in');
@@ -36,6 +46,9 @@ function showElement(element) {
 }
 
 function hideElement(element) {
+    // This function hides the element by adding the hidden class and removing the fade-in class
+    // As the animation shows and hides the elements, each element appears and gets hidden again
+
     if (element) {
         element.classList.remove('fade-in');
         element.classList.add('fade-out');
@@ -48,6 +61,9 @@ function hideElement(element) {
 }
 
 function finishAnimation() {
+    // This function moves the animation to its final state, bu adjusting the title
+    // and showing and positioning the subtitle and the startGame button
+
     if (title) {
         title.classList.add('final-state');
         title.style.marginTop = "20px";
@@ -68,6 +84,11 @@ function finishAnimation() {
 }
 
 function runAnimations() {
+
+    // This function runs the animations in sequence
+    // All the elements are shown and hidden one by one using a timeout
+    // The final state of the animation is reached by calling the finishAnimation function
+
     showElement(g9);
 
     setTimeout(() => {
@@ -95,9 +116,11 @@ function runAnimations() {
         }, 1000);
     }, 6000);
 }
-*/
-// Initiallations
 
+
+// The game logic
+
+// Initialization of the non-constant variables in the game
 let distance = 0;
 let difficulty = 50;
 let routesNum = 3;
@@ -112,7 +135,7 @@ let optimalPath;
 let end;
 let start;
 
-
+// Initialization of the constant game variables, the elements from the html
 const scoreText = document.querySelector('#distanceText');
 const easyButton = document.querySelector('#easy');
 const mediumButton = document.querySelector('#medium');
@@ -122,16 +145,11 @@ const routeNumber = document.querySelector('#routeNumber');
 const resetButton = document.querySelector('#reset');
 const distanceReset = document.querySelector('#distanceReset');
 const nextRoundButton = document.querySelector('#nextRoundButton');
-const startGame = document.querySelector('#startGame');
-const playAgainButton = document.querySelector('#playAgainButton');
 
+const playAgainButton = document.querySelector('#playAgainButton');
 const finalBarContainer = document.getElementById("finalBarContainer");
 
-// const progressBarContainer = document.getElementById("progressBarContainer");
-// const progressFill = document.getElementById("progressFill");
-// const progressText = document.getElementById("progressText");
-
-
+// Adding event listeners to the buttons
 easyButton.onclick = setDifficultyEasy;
 mediumButton.onclick = setDifficultyMedium;
 hardButton.onclick = setDifficultyHard;
@@ -143,28 +161,13 @@ playAgainButton.onclick = finishGame;
 routeNumber.addEventListener('input', updateRoutesNum);
 
 
-
 // Geting the information thorugh Flask
 
-function loadData(data){
-
-    blockedRoads = data["blocked nodes"];
-    neighbours = data["neighbours"];
-    optimalDistance = data["optimal distance"];
-    optimalPath = data["optimal path"];
-    end = data["end"];
-    start = data["start"];
-
-    console.log("Data loaded");
-    console.log("Blocked nodes:", blockedRoads);
-    console.log("Neighbours:", neighbours);
-    console.log("Optimal distance:", optimalDistance);
-    console.log("Optimal path:", optimalPath);
-    console.log("End:", end);
-    console.log("Start:", start);
-}
-
 async function initializeFlask() {
+
+    // This function fetches the data from the server by sending a POST request to the server and returns it
+    // This fetch is of the type start, and gets the data for drawing the map
+
     const start = {"type": "start", "difficulty": difficulty}
     try{
         const response = await fetch('http://127.0.0.1:5000/main',{
@@ -188,6 +191,10 @@ async function initializeFlask() {
 }
 
 async function requestNeighbours(coords) {
+
+    // This function works as the initialize flask function, but is of the type neighbours
+    // It sends the current coordinates and gets the adjacent coordinates (neighbours)
+
     const send_neighbours = {"type": "neighbours", "current": coords, "difficulty": difficulty}
     try{
         const response = await fetch('http://127.0.0.1:5000/main',{
@@ -212,10 +219,108 @@ async function requestNeighbours(coords) {
     }
 }
 
-// Rest of the functions
+// Event listener functions
+function setDifficultyEasy() {
 
-// This function loads new data and starts a new round
+    // This function sets the difficulty to easy, hides the menu and starts the game
+    // A difficulty value of 0 means there will be no blocked roads
+
+    difficulty = 0;
+    menu.style.display = "none";
+    initializeGame();
+}
+
+function setDifficultyMedium() {
+
+    // With a medium difficulty, the map will be generated with 50 blocked roads
+
+    difficulty = 50;
+    menu.style.display = "none";
+    initializeGame();
+}
+
+function setDifficultyHard() {
+
+    // Same functionality, 100 blocked roads 
+
+    difficulty = 100;
+    menu.style.display = "none";
+    initializeGame();
+}
+
+function resetGame() {
+
+    // This function resets the game by setting the distance to 0 and starting a new round
+    // If the progress bar is displayed, it gets hidden, and the map gets cleared
+    // In this case, startNewRound() will start the same round the player is currently playing
+
+    console.log("Resetting")
+
+    distance = 0;
+    scoreText.innerHTML = distance;
+    progressBarContainer.style.display = "none";
+    clearMap();
+    startNewRound();
+}
+
+function hideStartScreen() {
+
+    // This function hides the game explanation and starts the game
+
+    gameExplanation.style.display = "none";
+    distanceReset.style.display = "block";
+    menu.style.display = "block";
+}
+
+function updateRoutesNum() {
+
+    // This function updates the number of routes the player will play
+    // The value is converted to an integer and set to 3 (default) if it's out of range
+
+    const value = parseInt(routeNumber.value);
+
+    if (value >= 1 && value <= 10) {
+        routesNum = value;
+    } else {
+        routesNum = 3;
+    }
+}
+
+function nextRound() {
+
+    // This function gets called when the next round button is clicked
+    // It increments the current round and checks if the game is finished
+    // If it is, the final score is calculated and the progress bar is shown
+    // Otherwise, the progress bar is hidden and the next round starts
+
+    barContainer.style.display = "none";
+    currentRound++;
+    if (currentRound === routesNum) {
+        percentage = Math.round(100*(finalOptimalDistance/finalDistance));
+        showBar(percentage,"final");
+    }
+    else{
+        clearMap();
+        initializeGame();
+    }
+}
+
+function finishGame() {
+
+    // This function gets called when the play again button is clicked
+    // It reloads the page so the player can play again
+
+    console.log("Game finished");
+    location.reload();
+}
+
+// Helper functions (i.e. functions that are not event listeners and are called by others)
+
 async function initializeGame(){
+
+    // This function initializes the game by fetching the data from the server and loading it
+    // It has a try statement to catch any errors that might occur due to the server not responding
+    
     try{
         distance = 0;
         scoreText.innerHTML = distance;
@@ -230,62 +335,33 @@ async function initializeGame(){
     }
 }
 
-function setDifficultyEasy() {
-    difficulty = 0;
-    menu.style.display = "none";
-    initializeGame();
+function loadData(data){
+
+    // All the data gets loaded from a json dictionary (represented as an object in JS)
+
+    blockedRoads = data["blocked nodes"];
+    neighbours = data["neighbours"];
+    optimalDistance = data["optimal distance"];
+    optimalPath = data["optimal path"];
+    end = data["end"];
+    start = data["start"];
+    console.log("Data loaded");
 }
 
-function setDifficultyMedium() {
-    difficulty = 50;
-    menu.style.display = "none";
-    initializeGame();
-}
-
-function setDifficultyHard() {
-    difficulty = 100;
-    menu.style.display = "none";
-    initializeGame();
-}
-
-// This function resets the current round. It resets the distance, clears the map and restarts the current round.
-function resetGame() {
-    console.log("Resetting")
-
-    distance = 0;
-    scoreText.innerHTML = distance;
-    progressBarContainer.style.display = "none";
-    clearMap();
-    startNewRound();
-}
-
-// This function obtains the value that the user wrote on the box for the number or rountes and
-// If the user wrote nothing or an invalid number, it sets 3 as the default
-function updateRoutesNum() {
-    const value = parseInt(routeNumber.value);
-
-    if (value >= 1 && value <= 10) {
-        routesNum = value;
-    } else {
-        routesNum = 3;
-    }
-}
-
-function hideStartScreen() {
-    gameExplanation.style.display = "none";
-    distanceReset.style.display = "block";
-    menu.style.display = "block";
-}
-
-// Add addition to the current distance traversed for this round. Update the counter shown in the UI
 function updateDistance(addition) {
+
+    // This function updates the distance by adding the length of the road traversed
+
     console.log(addition, typeof(addition))
     distance += parseFloat(addition);
     scoreText.innerHTML = Math.round(distance).toString();
 }
 
-// This function updatse the cumulative distance and optimal distance across all rounds and calls to show a bar displaying how well the user did compared to the optimal distance
 function endRound() {
+
+    // This function gets called when the end node is reached, it calculates the final and optimal distances
+    // It also calculates the percentage of closeness (final score) and calls a function to show the score bar
+
     finalDistance += distance;
     finalOptimalDistance += optimalDistance;
     if(distance === 0){
@@ -293,12 +369,16 @@ function endRound() {
         return;
     }
 
-    percentage = Math.round(100*(optimalDistance/distance));
+    const percentage = Math.min(100, Math.round(100*(optimalDistance/distance)));
     showBar(percentage, "progress");
 }
 
-// This function shows a bar with a percentage. The particular bar to show is passed as an argument
 function showBar(percentage, bar) {
+
+    // This function shows the score of the round with a progress bar
+    // The percentage is calculated and the color of the bar changes depending on the percentage
+
+    // Variables for the elements of the progress bar
     barContainer = document.getElementById(bar+"BarContainer");
     barFill = document.getElementById(bar+"Fill");
     barText = document.getElementById(bar+"Text");
@@ -321,26 +401,4 @@ function showBar(percentage, bar) {
     }, 100);
 
     barText.textContent = `Score: ${percentage}%`;
-}
-
-function nextRound() {
-    console.log("Going into next round");
-    barContainer.style.display = "none";
-    currentRound++;
-    // If it's the final round, show a bar with the final performance
-    if (currentRound === routesNum) {
-        percentage = Math.round(100*(finalOptimalDistance/finalDistance));
-        showBar(percentage,"final");
-    }
-    // Otherwise, clear the map and initialise the game again for a new round
-    else{
-        clearMap();
-        initializeGame();
-    }
-}
-
-function finishGame() {
-    // A page will appear saying the game is finished and will have a button to reload the page to start again
-    console.log("Game finished");
-    location.reload();
 }
