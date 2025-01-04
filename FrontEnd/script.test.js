08649d8e4762a60bc0ba2d211063755973e6b31c
A. Lopez Ruiz <a.lopez.ruiz@umail.leidenuniv.nl>
2025-01-04 20:14:53 +0100

    change test to use vitest instead of jest

---
+++ FrontEnd/script.test.js
@@ -0,0 +1,4 @@
+import { describe, test, it, expect } from 'vitest';
+
+// This unittest is using vitest, which was improted in the termial using "npm i -D vitest"
+// The test is run by typing "npm test" in the terminal
