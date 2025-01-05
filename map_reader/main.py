3d81b9f70bb74052d1f4b2a1d1655d8f43be8ab3
L. Agbaria <l.agbaria@umail.leidenuniv.nl>
2025-01-05 01:59:30 +0100

    Fixed type-hinting and improved docstring, did not change actual code except removing extra whitespace.

--- map_reader/main.py
+++ map_reader/main.py
@@ -1,6 +1,7 @@
 from Map import Map
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, wrappers
 from flask_cors import CORS
+
 app = Flask(__name__)
 CORS(app)
 
@@ -12,40 +13,46 @@ if __name__ == "__main__":
 else:
     game = Map("map_test_5-1.json")
 
-def send_start(data: dict) -> dict:
+
+def send_start(data: dict) -> wrappers.Response:
     """
-    sends the start, end and blocked nodes to the UI in a JSON file
+    Sends the start, end and blocked nodes to the UI in a JSON file
     Keep tracks of the round to reset start and end when needed
-    :rtype dict:
+
+    :return (JSON): The starting data required to initiate the game.
     """
     game.game_init(data["difficulty"])
-    return jsonify({"start" : game.start, 
-                    "end" : game.end, 
+    return jsonify({"start": game.start, 
+                    "end": game.end, 
                     "blocked nodes": game.blocked_roads, 
                     "neighbours": game.get_neighbours_and_roads(game.start),
-                    "optimal path" :  game.optimal_path, 
-                    "optimal distance" : game.optimal_distance})
+                    "optimal path": game.optimal_path, 
+                    "optimal distance": game.optimal_distance})
 
-def send_neighbours(data: dict) -> dict:
+def send_neighbours(data: dict[str]) -> wrappers.Response:
     """
-    calls generate neighbours and sends to the UI a JSON file with them
-    :rtype dict:
+    Calls generate neighbours and sends to the UI a JSON file with them.
+
+    :param data (dict): The current game data.
+
+    :return (JSON): The neighbours of the current node.
     """
     return jsonify({"neighbours": game.get_neighbours_and_roads(data["current"])})
 
 
 @app.route('/main', methods=['POST'])
-def main() -> dict:
+def main() -> tuple[wrappers.Response, int]:
     """
-    processes the inputs from the webpage
-    :rtype dict:
+    Processes the inputs from the webpage.
+
+    :return (JSON): The results of the input processing.
     """
     data = request.get_json()
     
     if data["type"] == "start":
-        return send_start(data)
+        return send_start(data), 0
     elif data["type"] == "neighbours":
-        return send_neighbours(data)
+        return send_neighbours(data), 0
  
     
     return jsonify({"error" : "The data is not a JSON or the format is invalid"}), 400
